af077ada3a9a57bee30b349ec6d4f81c5ef4fd0c
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-09-24 17:20:30 +1000

    RS-8380: Attach some flip packages again so they work with auto-complete (#1)
    * RS-8380: Attach some pkgs instead of just loading them
    
    * Attach flipU, lubridate, flipTime, flipTransformations, flipChart,
    and verbs, so that their functions are availabile for use with
    auto-complete in Calculations
    
    * RS-8380: attach flipTables for Rbind,Cbind

--- R/listpackages.R
+++ R/listpackages.R
@@ -4,7 +4,9 @@ flip_list <- c("flipU", "flipTime", "flipTransformations", "verbs", "flipTables"
                "flipDashboard", "flipDimensionReduction", "flipRegression", "flipStartup",
                "flipTrees", "flipAnalysisOfVariance", "flipQualtrics", "flipMultivariates",
                "flipCluster", "flipTURF")
-
+attach_list <- c("flipU", "lubridate", "flipTime", "flipTransformations",
+                    "flipTables", "flipChart", "verbs")
+load_list <- setdiff(flip_list, attach_list)
 
 #' List Flip packages
 #' This function lists the flip packages that will be loaded
