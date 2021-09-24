cc7cac6ee7d07253a5c8bf73196268fbfe13cb68
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-09-24 10:47:54 +1000

    RS-8380: Attach some pkgs instead of just loading them
    * Attach flipU, lubridate, flipTime, flipTransformations, flipChart,
    and verbs, so that their functions are availability for use with
    auto-complete in Calculations

--- R/attach.R
+++ R/attach.R
@@ -1,28 +1,34 @@
 # This code was copied from tidyverse
 # The list of packages is stored in listpackages.R
 
-packages_unloaded <- function()
+packages_unloaded <- function(attach = FALSE)
 {
-  search <- paste0("package:", flip_list)
-  flip_list[!search %in% search()]
+    pkg_list <- if (attach) attach_list else load_list
+    search <- paste0("package:", pkg_list)
+    return(pkg_list[!search %in% search()])
 }
 
 attach_flip <- function()
 {
-  to_load <- packages_unloaded()
-  if (length(to_load) == 0)
-    return(invisible())
+    to_load <- packages_unloaded(FALSE)
+    to_attach <- packages_unloaded(TRUE)
+    if (length(to_load) == 0)
+      return(invisible())
 
-  successes <- suppressWarnings(suppressPackageStartupMessages(
-    vapply(to_load, requireNamespace, FALSE, quietly = TRUE)
-  ))
-  if (any(!successes))
-  {
-      unavailable_pkgs <- paste(names(successes)[!successes], collapse = ", ")
-      msg <- paste0("The following flip packages are not installed and were not load: ",
-                    unavailable_pkgs, ".")
-      warning(msg, call. = FALSE)
-  }
+    successes <- suppressWarnings(suppressPackageStartupMessages(
+        vapply(to_load, requireNamespace, FALSE, quietly = TRUE)
+    ))
+    successes <- c(successes, suppressWarnings(suppressPackageStartupMessages(
+        vapply(to_attach, require, FALSE, quietly = TRUE, character.only = TRUE)
+    )))
 
-  invisible()
+    if (any(!successes))
+    {
+        unavailable_pkgs <- paste(names(successes)[!successes], collapse = ", ")
+        msg <- paste0("The following flip packages are not installed and were not loaded: ",
+                      unavailable_pkgs, ".")
+        warning(msg, call. = FALSE)
+    }
+
+    invisible()
 }
